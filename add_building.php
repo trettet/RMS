807f8c954ebb5cdf93f17edcb8b820899f4b88f9
allenazamoras <allenazamoras@gmail.com>
2017-03-21 05:54:52 +0800

    Added functionalities in admin

---
+++ add_building.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	require("sql_connect.php");
+	
+	$building_name = $_POST["building_name"];
+	$address = $_POST["address"];
+	$contact_no = $_POST["contact_no"];
+	$desk_person = $_POST["desk_person"];
+	
+	$query = "INSERT INTO buildings (building_name, address, contact_no, desk_person_id) VALUES ('{$building_name}', '{$address}', '{$contact_no}', '{$desk_person}')";
+	
+	$insert = mysqli_query($conn, $query);
+	
+	if($insert){
+		echo "<script>alert('Building Successfully Added!');
+					  window.location.href='admin.php';</script>";
+	}else{
+		echo "<script>alert('Something went wrong, please try again.');
+					  window.location.href='admin.php';</script>";
+	}
+	
+?>
---
+++ add_dp.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+	require("sql_connect.php");
+	
+	if(strcasecmp($_POST["password"], $_POST["password2"]) == 0){
+		$password = $_POST["password"];
+	}else{
+		echo "<script>alert('Password does not match!');
+					  window.location.href='admin.php';</script>";
+	}
+	
+	$username = $_POST["username"];
+	$lname = $_POST["lname"];
+	$fname = $_POST["fname"];
+	$mname = $_POST["mname"];
+	$address = $_POST["address"];
+	$contact = $_POST["contact_no"];
+	
+	$query = "INSERT INTO users (username, password, lname, fname, mname, address, contact_no) VALUES ('{$username}', '{$password}', '{$lname}', '{$fname}', '{$mname}', '{$address}', '{$contact}')";
+	$insert = mysqli_query($conn, $query);
+	
+	if($insert){
+		echo "<script>alert('Desk Person Successfully Added!');
+					  window.location.href='admin.php';</script>";
+	}else{
+		echo "<script>alert('Something went wrong, please try again.');
+					  window.location.href='admin.php';</script>";
+	}
+?>
